4013c55cfe285028120fa84b1b7c40421849c2fc
Sanghoon Shin <shshin@gameon.co.jp>
2017-09-19 16:55:17 +0900

    first commit

---
+++ pandas_csv_test.py
@@ -0,0 +1,31 @@
+# CSV Example
+
+# sep=;
+# ;HOG;HOG;HOG;Day 1;Day 1;Day 1;Day 2;Day 2;Day 2;Day 3;Day 3;Day 3;Day 4;Day 4;Day 4;Day 5;Day 5;Day 5;Day 6;Day 6;Day 6;Day 7;Day 7;Day 7;Day 8;Day 8;Day 8;Day 9;Day 9;Day 9;Day 10;Day 10;Day 10;Day 11;Day 11;Day 11;Day 12;Day 12;Day 12;Day 13;Day 13;Day 13;Day 14;Day 14;Day 14;Day 15;Day 15;Day 15;Day 16;Day 16;Day 16;Day 17;Day 17;Day 17;Day 18;Day 18;Day 18;Day 19;Day 19;Day 19;Day 20;Day 20;Day 20;Day 21;Day 21;Day 21;Day 22;Day 22;Day 22;Day 23;Day 23;Day 23;Day 24;Day 24;Day 24;Day 25;Day 25;Day 25;Day 26;Day 26;Day 26;Day 27;Day 27;Day 27;Day 28;Day 28;Day 28;Day 29;Day 29;Day 29;Day 30;Day 30;Day 30;Day 31;Day 31;Day 31;Day 32;Day 32;Day 32;Day 33;Day 33;Day 33;Day 34;Day 34;Day 34;Day 35;Day 35;Day 35;Day 36;Day 36;Day 36;Day 37;Day 37;Day 37;Day 38;Day 38;Day 38;Day 39;Day 39;Day 39;Day 40;Day 40;Day 40;Day 41;Day 41;Day 41;Day 42;Day 42;Day 42;Day 43;Day 43;Day 43;Day 44;Day 44;Day 44;Day 45;Day 45;Day 45;Day 46;Day 46;Day 46;Day 47;Day 47;Day 47;Day 48;Day 48;Day 48;Day 49;Day 49;Day 49;Day 50;Day 50;Day 50;Day 51;Day 51;Day 51;Day 52;Day 52;Day 52;Day 53;Day 53;Day 53;Day 54;Day 54;Day 54;Day 55;Day 55;Day 55;Day 56;Day 56;Day 56;Day 57;Day 57;Day 57;Day 58;Day 58;Day 58;Day 59;Day 59;Day 59;Day 60;Day 60;Day 60;Day 61;Day 61;Day 61;Day 62;Day 62;Day 62;Day 63;Day 63;Day 63;Day 64;Day 64;Day 64;Day 65;Day 65;Day 65;Day 66;Day 66;Day 66;Day 67;Day 67;Day 67;Day 68;Day 68;Day 68;Day 69;Day 69;Day 69;Day 70;Day 70;Day 70;Day 71;Day 71;Day 71;Day 72;Day 72;Day 72;Day 73;Day 73;Day 73;Day 74;Day 74;Day 74;Day 75;Day 75;Day 75;Day 76;Day 76;Day 76;Day 77;Day 77;Day 77;Day 78;Day 78;Day 78;Day 79;Day 79;Day 79;Day 80;Day 80;Day 80;Day 81;Day 81;Day 81;Day 82;Day 82;Day 82;Day 83;Day 83;Day 83;Day 84;Day 84;Day 84;Day 85;Day 85;Day 85;Day 86;Day 86;Day 86;Day 87;Day 87;Day 87;Day 88;Day 88;Day 88;Day 89;Day 89;Day 89;Day 90;Day 90;Day 90
+# date;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total;ios;android;total
+# 2017/07/25;12,758;4,771;17,529;4,334;1,798;6,132;2,670;1,201;3,871;2,241;1,029;3,270;1,929;915;2,844;1,736;806;2,542;1,543;721;2,264;1,388;641;2,029;1,258;580;1,838;1,172;567;1,739;1,064;503;1,567;1,022;469;1,491;965;461;1,426;905;441;1,346;873;409;1,282;811;383;1,194;737;348;1,085;710;337;1,047;705;322;1,027;644;322;966;683;322;1,005;639;317;956;632;298;930;612;296;908;540;263;803;535;249;784;526;242;768;536;242;778;497;240;737;473;216;689;432;216;648;412;199;611;420;195;615;407;195;602;411;194;605;407;194;601;380;193;573;398;194;592;366;178;544;358;181;539;354;178;532;382;177;559;355;171;526;342;170;512;331;167;498;341;159;500;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0
+#
+
+import pandas as pd
+
+df = pd.read_csv('./test.csv',sep=';',header=2,thousands=',')
+
+del_cols = []
+for i in range(1, len(df.columns)):
+    if (i % 3 != 0):
+        del_cols.append(i)
+
+df = df.drop(df.columns[del_cols],axis=1)
+df = df.drop(df.columns[0], axis=1)
+
+df = df.drop(df.columns[len(df.index):], axis=1)
+
+column_sums = []
+
+for i in range(len(df.columns)):
+    column_sums.append(int(df[df.columns[i]].sum()))
+    
+','.join([str(v) for v in column_sums])
+
+first_column = [int(v) for v in df[df.columns[0]].tolist()]
+','.join([str(v) for v in first_column])
